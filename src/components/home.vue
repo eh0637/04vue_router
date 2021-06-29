b06f800400e78372e4abcaf433649c4d1810be12
eh0637 <kimeunh429@gmail.com>
2021-06-29 17:59:58 +0900

    up

--- src/components/home.vue
+++ src/components/home.vue
@@ -220,9 +220,9 @@ export default {
     }
     .btn a {color: #666; display: block; transition: 0.3s;}
     .btn a:hover{color: #333; text-shadow: 3px 3px 3px rgba(0,0,0,0.2);}
-  .swiper-button-prev{background-image: url(/images/prebtn.svg); background: #000;}
+  
   .swiper-pagination-progressbar .swiper-pagination-progressbar-fill {
-    background: #ea0b2a !important;
+    background: #333 !important;
     }
 .swiper-pagination-progressbar.swiper-pagination-progressbar-opposite  {
     width: 100%;
